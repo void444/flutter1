b85fb1aea622c052cfaa4066194aa6077b11b0b8
void444 <basantadhikari2001@gmail.com>
2023-07-16 11:29:32 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_summernote/flutter_summernote.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Demo Flutter Summernote'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({required this.title});
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey<FlutterSummernoteState> _keyEditor = GlobalKey();
+  String result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        elevation: 0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () async {
+              final value = (await _keyEditor.currentState?.getText());
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                duration: Duration(seconds: 5),
+                content: Text(value ?? '-'),
+              ));
+            },
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: FlutterSummernote(
+        hint: 'Your text here...',
+        key: _keyEditor,
+        hasAttachment: true,
+        customToolbar: """
+          [
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['insert', ['link', 'table', 'hr']]
+          ]
+        """,
+      ),
+    );
+  }
+}
